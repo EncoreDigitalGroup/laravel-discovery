462d0e224070e3a1389e4d5c22f3816a23fe2711
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-11-15 13:07:51 -0600

    Add `Discovery::path()` and Deprecate `Discovery::cache()` for `Discovery::get()` (#9)

--- src/Support/Discovery.php
+++ src/Support/Discovery.php
@@ -35,13 +35,28 @@ class Discovery
         return self::make()->config;
     }
 
-    public static function cache(string $key): array
+    public static function path(string $key): string
+    {
+        if (class_exists($key)) {
+            $key = class_basename($key);
+        }
+
+        return base_path("{$key}.php");
+    }
+
+    public static function get(string $key): array
     {
         if (class_exists($key)) {
             $key = class_basename($key);
         }
 
         return require base_path("{$key}.php");
+    }
+
+    /** @deprecated use Discovery::get() instead. */
+    public static function cache(string $key): array
+    {
+        return self::get($key);
     }
 
     /** @internal */
